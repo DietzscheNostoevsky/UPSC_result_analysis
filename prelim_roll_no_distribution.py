20e945b82575a600fecde2b052166b004b29ca93
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-06-22 20:20:55 +0530

    got distro in quali dict

--- prelim_roll_no_distribution.py
+++ prelim_roll_no_distribution.py
@@ -35,4 +35,22 @@ with file:
     write = csv.writer(file)
     write.writerow(quali)
 
+
+# %%
+quali_int = []
+for q in quali:
+    q = int(q)
+    quali_int.append(q)
+# %%
+for a in range(0, 10000000, 1000000):
+    print(a)
+
+# %%
+quali_dict = {}
+for a in range(0, 10000000, 1000000):
+    for num in quali_int:
+        if num > a and num < (a+1000000):
+            quali_dict[a] = quali_dict.get(a, 0) + 1
+
+
 # %%
